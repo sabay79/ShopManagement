70cec144e550995d143690b4526f90a9c6c935db
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-01-17 10:02:02 +0500

    Added Product Service - Cut/Paste List from controller to it

---
+++ src/ShopManagement/BusinessServices/SM.Business.DataServices/ProductService.cs
@@ -0,0 +1,15 @@
+﻿using SM.Business.Models;
+
+namespace SM.Business.DataServices
+{
+    public class ProductService
+    {
+        private List<ProductModel> products = new List<ProductModel>();
+        products.Add(new ProductModel { Id = 1, Name = "Product 1" });
+        products.Add(new ProductModel { Id = 2, Name = "Product 2" });
+        products.Add(new ProductModel { Id = 3, Name = "Product 3" });
+        products.Add(new ProductModel { Id = 4, Name = "Product 4" });
+        products.Add(new ProductModel { Id = 5, Name = "Product 5" });
+
+    }
+}
