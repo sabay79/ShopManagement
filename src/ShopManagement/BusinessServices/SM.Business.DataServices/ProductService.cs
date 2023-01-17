562b70f8057bc66761c1dbd76dcae6923a84035c
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-01-17 10:03:59 +0500

    Update ProductService.cs

--- src/ShopManagement/BusinessServices/SM.Business.DataServices/ProductService.cs
+++ src/ShopManagement/BusinessServices/SM.Business.DataServices/ProductService.cs
@@ -5,11 +5,15 @@ namespace SM.Business.DataServices
     public class ProductService
     {
         private List<ProductModel> products = new List<ProductModel>();
-        products.Add(new ProductModel { Id = 1, Name = "Product 1" });
-        products.Add(new ProductModel { Id = 2, Name = "Product 2" });
-        products.Add(new ProductModel { Id = 3, Name = "Product 3" });
-        products.Add(new ProductModel { Id = 4, Name = "Product 4" });
-        products.Add(new ProductModel { Id = 5, Name = "Product 5" });
+        public List<ProductModel> GetAll() 
+        {
+            products.Add(new ProductModel { Id = 1, Name = "Product 1" });
+            products.Add(new ProductModel { Id = 2, Name = "Product 2" });
+            products.Add(new ProductModel { Id = 3, Name = "Product 3" });
+            products.Add(new ProductModel { Id = 4, Name = "Product 4" });
+            products.Add(new ProductModel { Id = 5, Name = "Product 5" });
 
+            return products;
+        }
     }
 }
