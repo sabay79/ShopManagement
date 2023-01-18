cb4437edefa78b5ddd0778b8609129d6a76284c8
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-01-18 14:54:58 +0500

    Update ShopManagementDbContext.cs

--- src/ShopManagement/Repositories/SM.Data/ShopManagementDbContext.cs
+++ src/ShopManagement/Repositories/SM.Data/ShopManagementDbContext.cs
@@ -6,6 +6,8 @@ namespace SM.Data
     public class ShopManagementDbContext : DbContext
     {
         public ShopManagementDbContext(DbContextOptions<ShopManagementDbContext> options) : base(options) { }
+        
+        //All entities from Database as DBsets
         public DbSet<Product> Products { get; set; }
     }
 }
