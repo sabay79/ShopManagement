897299c9f02f9e1639cf0bd520d7b4f53a07c0ce
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-01-18 15:07:39 +0500

    Update ProductService.cs
    Direct Database access

--- src/ShopManagement/BusinessServices/SM.Business.DataServices/ProductService.cs
+++ src/ShopManagement/BusinessServices/SM.Business.DataServices/ProductService.cs
@@ -1,31 +1,34 @@
 ﻿using SM.Business.DataServices.Interfaces;
 using SM.Business.Models;
+using SM.Data;
 
 namespace SM.Business.DataServices
 {
     public class ProductService : IProductService
     {
-        private List<ProductModel> products = new List<ProductModel>();
+        private readonly ShopManagementDbContext _dbContext;
+        public ProductService(ShopManagementDbContext dbContext) 
+        {
+            _dbContext = dbContext;
+        }
         public List<ProductModel> GetAll()
         {
-            products.Add(new ProductModel { Id = 1, Name = "Product 1" });
-            products.Add(new ProductModel { Id = 2, Name = "Product 2" });
-            products.Add(new ProductModel { Id = 3, Name = "Product 3" });
-            products.Add(new ProductModel { Id = 4, Name = "Product 4" });
-            products.Add(new ProductModel { Id = 5, Name = "Product 5" });
-
-            return products;
+            var allProducts = _dbContext.Products.ToList();
+            var productModels = allProducts.Select(x => new ProductModel { Id = x.Id, Name = x.Name }).ToList(); 
+            return productModels;
         }
         public void Add(ProductModel model)
         {
-            products.Add(model);
+            _dbContext.Products.Add(new Data.Models.Product { Id = model.Id, Name=model.Name});
+            _dbContext.SaveChanges();
         }
         public void Delete(int id)
         {
-            var productToDelete = products.Where(x => x.Id == id).FirstOrDefault();
+            var productToDelete = _dbContext.Products.Where(x => x.Id == id).FirstOrDefault();
             if (productToDelete != null)
             {
-                products.Remove(productToDelete);
+                _dbContext.Products.Remove(productToDelete);
+                _dbContext.SaveChanges();
             }
         }
     }
